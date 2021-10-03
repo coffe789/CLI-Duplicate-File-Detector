c0871dc0a09b7bb16020db14c0c221ac2d1a2a2d
coffe789 <jonathan_davey@outlook.com>
2021-10-03 18:09:29 +0800

    Added hash function, header file, and main program

--- duplicates.c
+++ duplicates.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "duplicates.h"
 
 //Nothing here yet
 
 int main(int arc, char **argv)
 {
+	printf("%s\n", strSHA2("."));
 	return 0;
 }
---
+++ duplicates.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char *strSHA2(char *filename);
+extern int main(int argc, char **argv);
