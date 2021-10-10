3a290e5426c9f8a425ddd8652e5577de80fcc960
JJ <75469742+junjongwoo99@users.noreply.github.com>
2021-10-10 14:02:18 +0800

    Add files via upload

---
+++ getopt.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <getopt.h>
+
+#define OPTLIST "df"
+
+int main(int argc, char *argv[])
+{
+    int opt;
+    while ((opt = getopt(argc, argv, OPTLIST)) != -1)
+    {
+        switch (opt)
+        {
+        case ‘a’: //all file considered
+            break;
+        case ‘A’: //return EXIT_SUCCESS or EXIT_FAIRLURE
+            break;
+        case ‘f’:
+            break;
+        case ‘h’:
+            break;
+        case ‘l’:
+            break;
+        case ‘m’:
+            break;
+        case ‘q’:
+            break;
+        }
+    }
+
+    // optind is for the extra arguments
+    // which are not parsed
+    for (; optind < argc; optind++)
+    {
+        printf(“extra arguments
+               : % s\n”, argv[optind]);
+    }
+
+    return 0;
+}
