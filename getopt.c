c5c472c49fd48a9bf18e5952a1e031b1a8a548f2
JJ <75469742+junjongwoo99@users.noreply.github.com>
2021-10-13 15:03:48 +0800

    Update getopt.c

--- getopt.c
+++ getopt.c
@@ -1,3 +1,6 @@
+//  CITS2002 Project 2 2021
+//  Name(s):            Jonathan Davey, JJ Jun
+//  Student number(s):   22887169, 22763977
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -7,29 +10,38 @@
 #include <getopt.h>
 
 #define OPTLIST "Aaf:h:mlq"
+bool a, A, f, h, l, m, q = false; //optional flags
+char farg[1000];
+char harg[1000];
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *path[])
 {
     int opt;
     while ((opt = getopt(argc, argv, OPTLIST)) != -1)
     {
         switch (opt)
         {
-        case 'a': //all file considered
+        case 'a': //all files considered including . files
+            a = true;
             break;
-        case 'A': //return EXIT_SUCCESS or EXIT_FAIRLURE
+        case 'A':               //indicates if using advanced options by exiting
+            exit(EXIT_FAILURE); //we are not
+        case 'f':               //finds duplicates of argument file. Exits depending on if found
+            f = true;
+            strcpy(farg, strSHA2(optarg));
             break;
-        case 'f':
-	    printf("%s\n",optarg);
+        case 'h': //find all files with hash arg. Exits depending on if found
+            h = true;
+            strcpy(harg, optarg);
             break;
-        case 'h':
-	    printf("%s\n",optarg);
+        case 'l': //lists duplicates
+            l = true;
             break;
-        case 'l':
+        case 'm': //advanced
+            m = true;
             break;
-        case 'm':
-            break;
-        case 'q':
+        case 'q': //no printing. Exit depending on if duplicates found
+            q = true;
             break;
         }
     }
